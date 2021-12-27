02de875259ffda43650dcc7f413f0221554895d7
Vivek Hari <vivek.hari@outlook.co.nz>
2021-12-27 19:28:10 +1300

    Set up basic S3 static website.

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@
     -backend-config="bucket=<s3_bucket_name>" `
     -backend-config="region=<aws_region_name>" `
     -backend-config="dynamodb_table=<dynamodb_lock_table_name>"
+  
+  Ensure the following environment variables are set:
+  - TF_VAR_domain_name
 */
 
 terraform {
@@ -23,6 +26,26 @@ terraform {
   }
 }
 
+variable "domain_name" {
+  type        = string
+  description = "The domain name for the website excluding the 'www.' prefix."
+}
+
+locals {
+  www_s3_bucket_name = "www.${var.domain_name}"
+}
+
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "www_bucket" {
+  bucket = local.www_s3_bucket_name
+  policy = templatefile("build/templates/s3-policy.json", {
+    bucket = local.www_s3_bucket_name
+  })
+  website {
+    index_document = "index.html"
+  }
+  tags = {}
 }
