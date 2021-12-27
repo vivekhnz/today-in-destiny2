1736a148fb4e6a407ace13e86abef7b851ad22fd
Vivek Hari <vivek.hari@outlook.co.nz>
2021-12-27 16:05:19 +1300

    Fix Terraform DynamoDB lock key name.

--- build/bootstrap-terraform.ps1
+++ build/bootstrap-terraform.ps1
@@ -75,8 +75,8 @@ if ($tablesJson) {
         Write-Host "Creating Terraform lock state table..."
         & aws dynamodb create-table `
             --table-name $LockStateTableName `
-            --attribute-definitions 'AttributeName=LockId, AttributeType=S' `
-            --key-schema 'AttributeName=LockId, KeyType=HASH' `
+            --attribute-definitions 'AttributeName=LockID, AttributeType=S' `
+            --key-schema 'AttributeName=LockID, KeyType=HASH' `
             --provisioned-throughput 'ReadCapacityUnits=1, WriteCapacityUnits=1'
     }
 }
