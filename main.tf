b5f615668a5ce484e30c6c5737c2c69c3f05c51a
Vivek Hari <vivek.hari@outlook.co.nz>
2021-12-28 00:58:17 +1300

    Set up SSL certificate.

--- main.tf
+++ main.tf
@@ -91,3 +91,33 @@ resource "aws_route53_record" "root_a" {
     evaluate_target_health = false
   }
 }
+
+// SSL certificate
+resource "aws_acm_certificate" "ssl_certificate" {
+  domain_name               = var.domain_name
+  subject_alternative_names = [local.www_domain_name, "*.${var.domain_name}"]
+  validation_method         = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = local.common_tags
+}
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+  zone_id         = aws_route53_zone.site_zone.zone_id
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  ttl             = 60
+  allow_overwrite = true
+}
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.ssl_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
