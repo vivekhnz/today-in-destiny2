fd9b6ed9c8b10551df656a579fb3dd3466548394
Vivek Hari <vivek.hari@outlook.co.nz>
2021-12-28 19:32:15 +1300

    Replace static index HTML file with basic React app.

---
+++ app/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <div>Coming soon...</div>,
+    document.getElementById('root')
+);
---
+++ app/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({ extractComments: false })
+        ]
+    }
+}
