559e6fc302ff5c5b57dc4fa3cd7fe8f695a5f089
Vivek Hari <vivek.hari@outlook.co.nz>
2021-12-31 00:25:15 +1300

    Provision S3 bucket to hold JSON data files.

--- main.tf
+++ main.tf
@@ -34,9 +34,6 @@ variable "domain_name" {
 
 // Setup
 locals {
-  www_domain_name     = "www.${var.domain_name}"
-  www_s3_bucket_name  = local.www_domain_name
-  root_s3_bucket_name = var.domain_name
   common_tags = {
     Project = var.domain_name
   }
@@ -46,28 +43,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-// S3 buckets
-resource "aws_s3_bucket" "www_bucket" {
-  bucket = local.www_s3_bucket_name
-  policy = templatefile("build/templates/s3-policy.json", { bucket = local.www_s3_bucket_name })
-  website {
-    index_document = "index.html"
-  }
-  tags = local.common_tags
-}
-resource "aws_s3_bucket" "root_bucket" {
-  bucket = local.root_s3_bucket_name
-  policy = templatefile("build/templates/s3-policy.json", { bucket = local.root_s3_bucket_name })
-  website {
-    redirect_all_requests_to = "https://${local.www_domain_name}"
-  }
-  tags = local.common_tags
-}
-
 // SSL certificate
 resource "aws_acm_certificate" "ssl_certificate" {
   domain_name               = var.domain_name
-  subject_alternative_names = [local.www_domain_name, "*.${var.domain_name}"]
+  subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
   lifecycle {
     create_before_destroy = true
@@ -94,10 +73,67 @@ resource "aws_acm_certificate_validation" "cert_validation" {
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
+// static website S3 buckets
+locals {
+  www_s3_bucket_name  = "www.${var.domain_name}"
+  root_s3_bucket_name = var.domain_name
+}
+resource "aws_s3_bucket" "www_bucket" {
+  bucket = local.www_s3_bucket_name
+  policy = templatefile("build/templates/s3-policy.json", { bucket = local.www_s3_bucket_name })
+  website {
+    index_document = "index.html"
+  }
+  tags = local.common_tags
+}
+resource "aws_s3_bucket" "root_bucket" {
+  bucket = local.root_s3_bucket_name
+  policy = templatefile("build/templates/s3-policy.json", { bucket = local.root_s3_bucket_name })
+  website {
+    redirect_all_requests_to = "https://www.${var.domain_name}"
+  }
+  tags = local.common_tags
+}
+
+// S3 bucket to store data
+locals {
+  data_s3_bucket_name = "data.${var.domain_name}"
+}
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = local.data_s3_bucket_name
+  policy = templatefile("build/templates/s3-policy.json", { bucket = local.data_s3_bucket_name })
+  website {
+    index_document = "index.html"
+  }
+  tags = local.common_tags
+}
+resource "aws_s3_bucket_object" "data_index_html" {
+  bucket = local.data_s3_bucket_name
+  key    = "index.html"
+  source = "data/index.html"
+  etag   = filemd5("data/index.html")
+}
+resource "aws_s3_bucket_object" "data_d2_folder" {
+  bucket       = local.data_s3_bucket_name
+  key          = "d2/"
+  content_type = "application/x-directory"
+  lifecycle {
+    // don't destroy the folder as other services will upload files into it
+    prevent_destroy = true
+  }
+}
+resource "aws_s3_bucket_object" "data_d2_today_json" {
+  bucket = local.data_s3_bucket_name
+  key    = "d2/today.json"
+  source = "data/d2/today.json"
+  etag   = filemd5("data/d2/today.json")
+}
+
 // CloudFront distributions
 locals {
   www_s3_origin_id  = "S3-${local.www_s3_bucket_name}"
   root_s3_origin_id = "S3-${local.root_s3_bucket_name}"
+  data_s3_origin_id = "S3-${local.data_s3_bucket_name}"
 }
 resource "aws_cloudfront_distribution" "www_s3_distribution" {
   origin {
@@ -110,7 +146,7 @@ resource "aws_cloudfront_distribution" "www_s3_distribution" {
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
-  aliases = [local.www_domain_name]
+  aliases = ["www.${var.domain_name}"]
 
   enabled             = true
   is_ipv6_enabled     = true
@@ -187,6 +223,50 @@ resource "aws_cloudfront_distribution" "root_s3_distribution" {
 
   tags = local.common_tags
 }
+resource "aws_cloudfront_distribution" "data_s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.data_bucket.website_endpoint
+    origin_id   = local.data_s3_origin_id
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+  aliases = ["data.${var.domain_name}"]
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.data_s3_origin_id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.ssl_certificate.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2019"
+  }
+
+  tags = local.common_tags
+}
 
 // Route 53 configuration
 resource "aws_route53_zone" "site_zone" {
@@ -199,7 +279,7 @@ resource "aws_route53_zone" "site_zone" {
 }
 resource "aws_route53_record" "www_a" {
   zone_id = aws_route53_zone.site_zone.zone_id
-  name    = local.www_domain_name
+  name    = "www.${var.domain_name}"
   type    = "A"
   alias {
     name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
@@ -214,6 +294,16 @@ resource "aws_route53_record" "root_a" {
   alias {
     name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+resource "aws_route53_record" "data_a" {
+  zone_id = aws_route53_zone.site_zone.zone_id
+  name    = "data.${var.domain_name}"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.data_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.data_s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
