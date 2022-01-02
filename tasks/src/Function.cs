c8a7e67a10eba9c62c50131a2da0717daebb4402
Vivek Hari <vivek.hari@outlook.co.nz>
2022-01-02 21:49:55 +1300

    Add basic .NET 6 lambda function.

---
+++ tasks/src/Function.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Amazon.Lambda.Core;
+
+namespace TodayInDestiny2.Tasks;
+
+public class Function
+{
+    public void FunctionHandler(Stream stream, ILambdaContext ctx)
+    {
+        Console.WriteLine("Hello from Lambda!");
+
+        var envVars = Environment.GetEnvironmentVariables();
+        Console.WriteLine($"Environment Variables: {JsonSerializer.Serialize(envVars)}");
+        Console.WriteLine($"Function name: {ctx.FunctionName}");
+        Console.WriteLine($"Max mem allocated: {ctx.MemoryLimitInMB}");
+        Console.WriteLine($"Time remaining: {ctx.RemainingTime}");
+        Console.WriteLine($"CloudWatch log stream name: {ctx.LogStreamName}");
+        Console.WriteLine($"CloudWatch log group name: {ctx.LogGroupName}");
+    }
+}
