9809908fa0859f9721acceec4043aeef32fe1f0e
Vivek Hari <vivek.hari@outlook.co.nz>
2022-01-03 02:48:39 +1300

    Fix typo.

--- build/build-tasks-image.ps1
+++ build/build-tasks-image.ps1
@@ -8,9 +8,10 @@ $ErrorActionPreference = 'Stop'
 $workingDir = (Resolve-Path -Path "$PSScriptRoot/../tasks").Path
 Push-Location -Path $workingDir
 
+$nameTag = $null
 try {
     $nameTag = "today-in-destiny2-tasks:$BuildVersion"
-    & docker build --taag $nameTag . --build-arg "BUILD_VERSION=$BuildVersion" | Out-Host
+    & docker build --tag $nameTag . --build-arg "BUILD_VERSION=$BuildVersion" | Out-Host
 
     $res = & docker image inspect $nameTag
     if (!($res | ConvertFrom-Json)) {
